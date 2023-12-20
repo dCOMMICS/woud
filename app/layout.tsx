3896f9b547ea3c20f0fc7d853076bc0a55df10e4
dCOMMICS <sanwurld@gmail.com>
2023-12-20 23:35:54 +0300

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
-
+import Navbar from '@/components/Navbar'
 
 
 export const metadata: Metadata = {
@@ -17,8 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-
-        {children}
+        <Navbar />
+<main>
+  {children}
+</main>
+        
 
         </body>
     </html>
